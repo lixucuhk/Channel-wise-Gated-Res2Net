8fc11ff81ef2ed2de0244213413dde033af11165
LI Xu <44832757+lixucuhk@users.noreply.github.com>
2021-07-20 22:06:17 +0800

    Update start_training_evaluation.sh

--- start_training_evaluation.sh
+++ start_training_evaluation.sh
@@ -3,7 +3,7 @@ mkdir -p logs || exit 1
 
 randomseed=0 # 0, 1, 2, ...
 modelconfig=conf/training_mdl/se_mcg_res2net50_cesoftmax.json # configuration files in conf/training_mdl
-feats=la_lfcc  # `pa_spec`, `pa_cqt`, `pa_lfcc`, `la_spec`, `la_cqt` or `la_lfcc`
+feats=la_lfcc  # `la_spec`, `la_cqt` or `la_lfcc`
 runid=MCG-Res2Net50-CESoftmax-LALFCC0
 pretrained="none"
 . ./parse_options.sh || exit 1
